ce4c37d80b783588be395a3895271f0a86b7c4a7
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2021-07-10 12:06:04 +0200

    pointless

--- client/apiHooks/swr.ts
+++ client/apiHooks/swr.ts
@@ -9,6 +9,7 @@ export async function fetchJson(url: string) {
     return fetch(url).then((result) => result.json())
 }
 
+// Not used
 function delay(timeout: number): Promise<void> {
     return new Promise((resolve) => {
         setTimeout(resolve, timeout)
