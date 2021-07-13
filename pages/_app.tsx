55773d5eaec2843cae48d9e36c372355963a00e7
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2021-07-13 09:26:10 +0200

    Update _app.tsx
    meta no-index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "client/styles/globals.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
+import { Fragment } from "react"
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return <Fragment>
+        <Head>
+            <meta name="robots" content="noindex">
+        </Head>
+        <Component {...pageProps} />
+    </Fragment>
 }
 export default MyApp
