435b244534c56b029bcaced968f45fdecedc8635
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2021-07-14 18:04:18 +0200

    Update cookie.ts

--- pages/api/cookie.ts
+++ pages/api/cookie.ts
@@ -4,7 +4,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    //res.setHeader("Access-Control-Allow-Credentials", "true")
+    res.setHeader("Access-Control-Allow-Credentials", "true")
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080")
     res.send(req.cookies)
 }
