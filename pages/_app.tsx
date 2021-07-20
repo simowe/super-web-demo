1d61ce8516dcda1e78d43cf8205bbaf6bc4d06df
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2021-07-20 14:28:23 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { Fragment } from "react"
 function MyApp({ Component, pageProps }: AppProps) {
     return <Fragment>
         <Head>
-            
+            <meta httpEquiv="Strict-Transport-Security" content="max-age=60" />
             <meta name="robots" content="noindex" />
         </Head>
         <Component {...pageProps} />
