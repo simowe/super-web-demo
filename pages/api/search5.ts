aee1ca11f91c8f97dc7096804691a9744fd0ddb1
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2021-07-22 21:45:00 +0200

    trying a bunch of searches

---
+++ pages/api/search5.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import { benchmark } from "server/benchmark"
+import { getMoviesCollection } from "server/mongo"
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    const { query } = req.query
+
+    const result = await fetchSearchResults(query as string)
+    res.status(200).json(result)
+}
+
+export async function fetchSearchResults(query?: string) {
+    const movies = await getMoviesCollection()
+
+    const searchAggregation = {
+        $search: {
+            index: "movies",
+            phrase: {
+                query,
+                path: "title",
+            },
+        },
+    }
+
+    const data = await benchmark("Atlas search", async () =>
+        movies.aggregate([searchAggregation, { $limit: 50 }]).toArray()
+    )
+
+    return { data }
+}
